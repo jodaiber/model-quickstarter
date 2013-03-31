1a0b00367d27a4d12d8e29d4c8af1717f675691f
pablomendes <pablomendes@gmail.com>
2013-03-31 23:19:34 +0200

    added portuguese; added check for maven 3

--- run.sh
+++ run.sh
@@ -6,9 +6,18 @@ chmod +x index_db.sh
 mkdir wdir
 mkdir models
 
+mvn_version=`mvn -version | grep "Apache Maven" | sed -r "s/Apache Maven (\S+).+/\1/"`
+if [[ "$mvn_version" > "3.0.0" ]]; then
+   echo "Maven 3 ok."
+else         
+   echo "One of our dependencies requires Maven 3. Please check your maven version with 'mvn -version'" 
+   exit -1
+fi
+
+
 echo "Downloading OpenNLP models..."
 
-for l in en de nl ; do
+for l in en de nl pt ; do
     cd $l
     wget -q http://opennlp.sourceforge.net/models-1.5/$l-token.bin
     wget -q http://opennlp.sourceforge.net/models-1.5/$l-sent.bin
@@ -28,3 +37,4 @@ echo "Indexing..."
 
 #No OpenNLP models:
 ./index_db.sh wdir fr_FR fr/stopwords.list French models/fr
+./index_db.sh wdir pt_BR pt/stopwords.list Portuguese models/pt
